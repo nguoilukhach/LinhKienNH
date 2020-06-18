35b0235d6bb7d65d2e9a9f7d4d9d6feaa00dd8c2
Nguyen Van Hung <hungth230297@gmail.com>
2020-06-18 18:38:55 +0700

    First Day : Create Entity

---
+++ LinhKienNH.Data/EF/ShopDbContext.cs
@@ -0,0 +1,20 @@
+﻿using LinhKienNH.Data.EF.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinhKienNH.Data.EF
+{
+    public class ShopDbContext : DbContext
+    {
+        public ShopDbContext( DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
+ 
